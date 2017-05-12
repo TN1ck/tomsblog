3ed4d689a030d26b7e8166e93ba3c92d1cce2f77
TN1ck <tomwanick@gmail.com>
2017-05-12 16:43:13 +0200

    async font loading, smaller images

--- _includes/main.js
+++ _includes/main.js
@@ -4,3 +4,19 @@ document.querySelectorAll('.post-image-container').forEach(function (img) {
         this.classList.toggle('post-image-container--zoomed');
     });
 });
+
+
+// load fonts async
+var WebFontConfig = {
+    google: {
+        families: [ 'Noto+Sans:400,600,400italic' ]
+    },
+    timeout: 3000
+};
+
+(function(d) {
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = 'true';
+    s.parentNode.insertBefore(wf, s);
+})(document);
